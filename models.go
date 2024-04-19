dd41d7efd8cc0e47152eca13dd1c17e07d63c850
JeffSmith <jeff@seventhbeam.com>
2024-04-19 15:59:08 +1000

    Add fee amount to the Transaction Model

--- models.go
+++ models.go
@@ -114,6 +114,7 @@ type Transaction struct {
 	ParentId   string      `json:"parent_id"`
 	OccurredAt string      `json:"occurred_at"`
 	Amount     json.Number `json:"amount"`
+	FeeAmount  json.Number `json:"fee_amount"`
 	Balance    json.Number `json:"running_balance_amount"`
 	Reference  string      `json:"reference"`
 }
